6dc37b686860e085f360afec32d4a52620aff763
Hak9 <41369094+xHak9x@users.noreply.github.com>
2018-09-15 20:25:17 +0200

    Update socialphish.sh

--- socialphish.sh
+++ socialphish.sh
@@ -212,9 +212,6 @@ printf "\e[1;77m          \/            \/        \/                   \/       
 printf "\n"
 printf "\e[1;93m       .:.:.\e[0m\e[1;77m Phishing Tool coded by: @Hak9 \e[0m\e[1;93m.:.:.\e[0m\n"
 printf "\n"
-printf "  \e[101m\e[1;77m:: Disclaimer: Developers assume no liability and are not    ::\e[0m\n"
-printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by SocialPhish ::\e[0m\n"
-printf "\n"
 }
 
 createpage() {
